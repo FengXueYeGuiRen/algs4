6e267ed4696914e9ac227856abab5f15d00105bd
FXYGR <FXYGR@pm.me>
2020-01-08 10:32:58 +0800

    图(第4章), 拓扑排序(4.2.4.3 算法4.5)

--- src/main/java/edu/princeton/cs/algs4/zh/cn/graph/directed/Topological.java
+++ src/main/java/edu/princeton/cs/algs4/zh/cn/graph/directed/Topological.java
@@ -1,9 +1,35 @@
 package edu.princeton.cs.algs4.zh.cn.graph.directed;
 
 /**
- * description
+ * 拓扑排序 API(4.2.4.3)
  *
  * @author FXYGR @date 2020-01-08
  */
-public class Topological {
+public abstract class Topological {
+
+	public Topological() {
+	}
+
+	/**
+	 * 拓扑排序的构造函数
+	 *
+	 * @param G
+	 */
+	public Topological(DirectedGraph G) {
+	}
+
+	/**
+	 * G 是有向无环图吗
+	 *
+	 * @return
+	 */
+	public abstract boolean isDAG();
+
+	/**
+	 * 拓扑有序的所有顶点
+	 *
+	 * @return
+	 */
+	public abstract Iterable<Integer> order();
+
 }
--- src/main/java/edu/princeton/cs/algs4/zh/cn/graph/directed/TopologicalSort.java
+++ src/main/java/edu/princeton/cs/algs4/zh/cn/graph/directed/TopologicalSort.java
@@ -1,9 +1,51 @@
 package edu.princeton.cs.algs4.zh.cn.graph.directed;
 
 /**
- * description
+ * 拓扑排序(4.2.4.3 算法4.5)
+ * {@link edu.princeton.cs.algs4.Topological}
  *
  * @author FXYGR @date 2020-01-08
  */
-public class TopologicalSort {
+public class TopologicalSort extends Topological {
+
+	/**
+	 * 顶点的拓扑顺序
+	 */
+	private Iterable<Integer> order;
+
+	/**
+	 * 拓扑排序的构造函数
+	 *
+	 * @param G
+	 */
+	public TopologicalSort(DirectedGraph G) {
+		DirectedCycle directedCycle = new DefaultDirectedCycle(G);
+		if (directedCycle.hasCycle()) {
+			return;
+		}
+		DepthFirstSearchVertexOrder depthFirstOrder =
+				new DepthFirstSearchVertexOrder(G);
+		order = depthFirstOrder.reversePost();
+	}
+
+	/**
+	 * G 是有向无环图吗
+	 *
+	 * @return
+	 */
+	@Override
+	public boolean isDAG() {
+		return order != null;
+	}
+
+	/**
+	 * 拓扑有序的所有顶点
+	 *
+	 * @return
+	 */
+	@Override
+	public Iterable<Integer> order() {
+		return order;
+	}
+
 }
